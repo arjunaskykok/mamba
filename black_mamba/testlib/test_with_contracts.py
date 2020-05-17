cfda0b2ac1444ad43cd7ac837cd4492f09374ee6
Arjuna Sky Kok <arjuna@mamba.black>
2020-05-17 22:35:03 +0700

    Used rmtree to remove tests directory.

--- black_mamba/testlib/test_with_contracts.py
+++ black_mamba/testlib/test_with_contracts.py
@@ -1,15 +1,18 @@
 from pathlib import Path
+from shutil import rmtree
 
 
 class TestWithContracts():
 
-    tmp_path = Path('/tmp') # TODO: Change this into dynamic temp directory
+    tmp_path = Path('/tmp/mamba_tests/') # TODO: Change this into dynamic temp directory
 
     def setup_method(self, method):
+        self.tmp_path.mkdir()
         self.fixtures_dir = Path('.') / Path('test') / Path('fixtures')
         self.build_dir = self.tmp_path / Path('build')
         self.migrations_dir = self.tmp_path / Path("migrations")
         self.decentralized_app_dir = self.tmp_path / Path("decentralized_app")
+        self.contracts_dir = self.tmp_path / Path("contracts")
         self.build_contracts_dir = self.tmp_path / Path('build') / Path('contracts')
         self.json_compiled_fixture = self.fixtures_dir / Path("HelloWorld.json")
         self.json_compiled_file = self.build_contracts_dir / Path('HelloWorld.json')
@@ -29,21 +32,4 @@ class TestWithContracts():
             self.decentralized_app_dir.mkdir()
 
     def teardown_method(self, method):
-        if self.json_compiled_file.exists():
-            self.json_compiled_file.unlink()
-        if self.json2_compiled_file.exists():
-            self.json2_compiled_file.unlink()
-        if self.migration_file.exists():
-            self.migration_file.unlink()
-        if self.migration2_file.exists():
-            self.migration2_file.unlink()
-        if self.build_contracts_dir.exists():
-            self.build_contracts_dir.rmdir()
-        if self.build_dir.exists():
-            self.build_dir.rmdir()
-        if self.migrations_dir.exists():
-            self.migrations_dir.rmdir()
-        if self.decentralized_app_dir.exists():
-            self.decentralized_app_dir.rmdir()
-        if self.keyfile2_file.exists():
-            self.keyfile2_file.unlink()
+        rmtree(self.tmp_path)
