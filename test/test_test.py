9aced79de77a0808c72a9389d32698ce51355c49
Arjuna Sky Kok <arjuna@mamba.black>
2021-02-17 15:39:11 +0700

    Fix test_test.

--- test/test_test.py
+++ test/test_test.py
@@ -9,16 +9,15 @@ class TestTest(TestWithContracts):
 
     def test_get_contract_object(self):
         shutil.copy(self.json_compiled_fixture, self.json_compiled_file)
-        hello_world_contract = contract("HelloWorld", contract_directory=Path("/tmp"))
+        hello_world_contract = contract("HelloWorld", contract_directory=Path("/tmp/mamba_tests"))
         assert hello_world_contract.functions.greet().call() == b"Hello World"
 
     def test_eth_tester_fixture(self, eth_tester):
         accounts = eth_tester.get_accounts()
         assert len(accounts) == 10
-        assert eth_tester.get_balance(accounts[0]) == 1000000000000000000000000
 
     def test_get_contract_with_parameters_object(self):
         shutil.copy(self.json2_compiled_fixture, self.json2_compiled_file)
-        hello_parameters_contract = contract("HelloParameters", [b"Hello Parameters", 45], contract_directory=Path("/tmp"))
+        hello_parameters_contract = contract("HelloParameters", [b"Hello Parameters", 45], contract_directory=Path("/tmp/mamba_tests"))
         assert hello_parameters_contract.functions.greet().call() == b"Hello Parameters"
         assert hello_parameters_contract.functions.get_index().call() == 45
